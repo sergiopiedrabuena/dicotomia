de46a6d72344e930ee7d33c24ab340d314f53a52
serghi.p <piedrabuena.sergio.f@gmail.com>
2022-12-04 17:33:22 -0300

    fix portada

--- src/containers/Portada.jsx
+++ src/containers/Portada.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import '@styles/Portada.scss';
 import FotoPortada from '@images/portada.png';
 
-import AnimatedBackground from '@components/AnimatedBackground';
-
 const Portada = () => {
 	return (
 		<>
-			<div id='portadaContainer'>
-				<AnimatedBackground></AnimatedBackground>				
+			<div id='portadaContainer'>			
 				<div id='textoContainer'>
-					<h1 id='tituloPortada' class='textoPortada'>Desarrollador web</h1>
-					<h3 id='subtituloPortada' class='textoPortada'>Desarrollo fullstack</h3>
+					<h1 id='tituloPortada' className='textoPortada'>Desarrollador web</h1>
+					<h3 id='subtituloPortada' className='textoPortada'>Desarrollo fullstack</h3>
 				</div>
 				<div id='fotoContainer'>
 					<img id='fotoPortada' src={FotoPortada}></img>
