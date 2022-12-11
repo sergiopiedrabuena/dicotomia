da86aa6ab6afd3b3e9f84c45c7e7f40b8288e2b5
serghi.p <piedrabuena.sergio.f@gmail.com>
2022-12-11 19:42:47 -0300

    change github contact: serghip for sergiopiedrabuena

--- src/containers/Contacto.jsx
+++ src/containers/Contacto.jsx
@@ -10,7 +10,7 @@ const Contacto = () => {
 			<footer id="contenidoContacto" className='contenidoContacto'>
 				<h1 className='tituloContacto'> CONTACTO </h1>
 				<div id='contenidoRedes'>
-					<div className='redesAcercaDe' id='linkedin'><a href='https://github.com/serghip'><img src={Icon1}></img></a></div>
+					<div className='redesAcercaDe' id='linkedin'><a href='https://github.com/sergiopiedrabuena'><img src={Icon1}></img></a></div>
 					<div className='redesAcercaDe' id='github'><a href='mailto:piedrabuena.sergio.f@gmail.com'><img src={Icon2}></img></a></div>
 					<div className='redesAcercaDe' id='gmail'><a href='https://www.linkedin.com/in/sergio-fabian-piedrabuena-37aa0122b/'><img src={Icon3}></img></a></div>
 				</div>
